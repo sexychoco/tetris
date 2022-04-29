776dca5260ad36dc9c405fcde3c50007ec3a433a
sexychoco <gaeun819@naver.com>
2022-04-29 13:03:00 +0900

    add features

--- js/tetris.js
+++ js/tetris.js
@@ -1,5 +1,10 @@
+import BLOCKS from "./blocks.js";
+
 //DOM
 const playground = document.querySelector(".playground>ul");
+const gameText = document.querySelector(".game_text");
+const scoreDisplay = document.querySelector(".score");
+const restartBtn = document.querySelector(".game_text>button");
 
 // Settings
 const GAME_ROWS = 20;
@@ -10,20 +15,6 @@ let score = 0;
 let duration = 500;
 let downInterval;
 let tempMovingItem;
-
-const BLOCKS = {
-  tree: [
-    [
-      [0, 0],
-      [0, 1],
-      [1, 0],
-      [1, 1],
-    ],
-    [],
-    [],
-    [],
-  ],
-};
 
 const movingItem = {
   type: "",
@@ -36,9 +27,11 @@ init();
 
 //functions
 function init() {
+  tempMovingItem = { ...movingItem };
   for (let i = 0; i < GAME_ROWS; i++) {
     prependNewLine();
   }
+  generateNewBlock();
 }
 
 function prependNewLine() {
@@ -52,4 +45,142 @@ function prependNewLine() {
   playground.prepend(li);
 }
 
-function renderBlocks() {}
+function renderBlocks(moveType = "") {
+  const { type, direction, top, left } = tempMovingItem;
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove(type, "moving");
+  });
+  BLOCKS[type][direction].some((block) => {
+    const x = block[0] + left;
+    const y = block[1] + top;
+    const target = playground.childNodes[y]
+      ? playground.childNodes[y].childNodes[0].childNodes[x]
+      : null;
+    const isAvailable = checkEmpty(target);
+    if (isAvailable) {
+      target.classList.add(type, "moving");
+    } else {
+      tempMovingItem = { ...movingItem };
+      if (moveType === "retry") {
+        clearInterval(downInterval);
+        showGameoverText();
+      }
+      setTimeout(() => {
+        renderBlocks("retry");
+        if (moveType === "top") {
+          seizeBlock();
+        }
+      }, 0);
+      return true;
+    }
+  });
+  movingItem.left = left;
+  movingItem.top = top;
+  movingItem.direction = direction;
+}
+
+function seizeBlock() {
+  const movingBlocks = document.querySelectorAll(".moving");
+  movingBlocks.forEach((moving) => {
+    moving.classList.remove("moving");
+    moving.classList.add("seized");
+  });
+  chekcMatch();
+}
+
+function chekcMatch() {
+  const childNodes = playground.childNodes;
+  childNodes.forEach((child) => {
+    let matched = true;
+    child.children[0].childNodes.forEach((li) => {
+      if (!li.classList.contains("seized")) {
+        matched = false;
+      }
+    });
+    if (matched) {
+      child.remove();
+      prependNewLine();
+      score++;
+      scoreDisplay.innerHTML = score;
+    }
+  });
+
+  generateNewBlock();
+}
+
+function generateNewBlock() {
+  clearInterval(downInterval);
+  downInterval = setInterval(() => {
+    moveBlock("top", 1);
+  }, duration);
+
+  const blockArray = Object.entries(BLOCKS);
+  const randomIndex = Math.floor(Math.random() * blockArray.length);
+  movingItem.type = blockArray[randomIndex][0];
+  movingItem.top = 0;
+  movingItem.left = 3;
+  movingItem.direction = 0;
+  tempMovingItem = { ...movingItem };
+  renderBlocks();
+}
+
+function checkEmpty(target) {
+  if (!target || target.classList.contains("seized")) {
+    return false;
+  }
+  return true;
+}
+
+function moveBlock(moveType, amount) {
+  tempMovingItem[moveType] += amount;
+  renderBlocks(moveType);
+}
+
+function changeDirection() {
+  const direction = tempMovingItem.direction;
+  direction === 3
+    ? (tempMovingItem.direction = 0)
+    : (tempMovingItem.direction += 1);
+  renderBlocks();
+}
+
+function dropBlock() {
+  clearInterval(downInterval);
+  downInterval = setInterval(() => {
+    moveBlock("top", 1);
+  }, 10);
+}
+
+function showGameoverText() {
+  gameText.style.display = "flex";
+}
+
+//event handling
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 39:
+      moveBlock("left", 1);
+      break;
+    case 37:
+      moveBlock("left", -1);
+      break;
+    case 40:
+      moveBlock("top", 1);
+      break;
+    case 32:
+      changeDirection();
+      break;
+    case 38:
+      dropBlock();
+      break;
+    default:
+      break;
+  }
+});
+
+restartBtn.addEventListener("click", () => {
+  playground.innerHTML = "";
+  gameText.style.display = "none";
+  init();
+});
